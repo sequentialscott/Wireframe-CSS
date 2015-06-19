8e9ef164fd53e931f2db2810cec0efa439602e3e
Scott McDaniel <scott_mcd@hotmail.com>
2015-06-19 17:21:22 -0400

    Initial Commit
    Initial Commit

---
+++ js/directives.js
@@ -0,0 +1,78 @@
+angular.module('wfDirectives', []);
+/* This module contains the Angular directives available across the site.
+     In the application to use them, make sure that "wfDirectives" is injected, like so:
+
+     angular.module('appName', ["wfDirectives"]);
+
+     Include this file after the angular.js file. */
+
+/* -------------------------- USER MESSAGES ---------------------------- */
+// Use this for user messages on Angular pages that required animation or dismiss options.
+// message-class: String with the class name saying what kind of message it is (success, error, etc.).  Can also be done in regular class attribute.
+// dismissable: Set this attribute to false if you don't want to let users dismiss the message.  Otherwise it is dismissable.
+// highlight: Include to animate the message's display with a color that fades out.  No value required.
+// time-to-show: Number of milliseconds to display the message before it fades out on its own.
+// on-dismiss: Callback function to be executed when dismissing the message.  Often used to reset the show message flag in the controller.
+angular.module('wfDirectives').directive('wfMessage', function () {
+    return {
+        restrict: 'AE',
+        scope: {
+			visible: '=',			// Object from controller with Boolean to control visibility
+            onDismiss: '&',         // Function to call on dismissal
+            msgType: '='   			// To monitor the type of message (success, warning, etc.) and update it if it changes.
+        },
+        replace: true,
+        transclude: true,
+        template: 
+			'<p class="message {{ msgType }}" data-ng-show="visible.show">' + 
+				'<span class="icon iconX secondary compact" data-ng-if="isDismissable" data-ng-click="dismiss()">X</span>' + 
+				'<span ng-transclude></span>' + 
+			'</p>',
+        link: function (scope, element, attrs) {
+			scope.isDismissable = 'dismissable' in attrs;
+			
+            // Dismissing
+			scope.dismiss = function(){
+				if (scope.onDismiss) scope.onDismiss();
+				scope.visible = false;
+			};
+
+        }
+    };
+});
+
+
+/* --------------------------- EXPAND & COLLAPSE ------------------------------- */
+// Depends on the data-forid attribute to point to the element to be toggled. Syntax is:
+// <div class="collapse-toggle" coc-utilities data-forid="target-id"></div>
+angular.module('wfDirectives').directive('wfCollapse', function () {
+    return {
+		restrict: 'AE',
+		transclude: true,
+		replace: true,
+		template: 
+			'<div>' + 
+				'<div class="collapse-toggle" data-ng-click="toggle()" data-ng-class="{collapsed: !expanded}"></div>' + 
+				'<div data-ng-show="expanded">' + 
+					'<div data-ng-transclude></div>' + 
+				'</div>' +
+			'</div>',
+        link: function (scope, element, attrs) {
+			scope.expanded = !('closed' in attrs);
+			scope.toggle = function(){
+				scope.expanded = !scope.expanded;
+			};
+
+        }
+    };
+});
+
+
+/* -------------------- LOADING INDICATOR ------------------------------------ */
+angular.module('wfDirectives').directive('wfLoading', function () {
+    return {
+        restrict: 'AE',
+        replace: 'true',
+        template: '<div class="spinner"><div class="rect1"></div><div class="rect2"></div><div class="rect3"></div><div class="rect4"></div><div class="rect5"></div></div>'
+    }
+});
---
+++ js/styleguide.js
@@ -0,0 +1,37 @@
+/* Contents for the style guide */
+var styles = [
+    { title: "Buttons", id: "buttons" },
+    { title: "Expand & Collapse", id: "collapse" },
+    { title: "Loading Indicator", id: "loading" },
+    { title: "Text Styles", id: "textStyles" },
+	{ title: "User Messages", id: "userMessages" }
+];
+
+
+/* Angular module and controller for style guide */
+var styleguide = angular.module('styleguide', ['wfDirectives']);
+
+styleguide.controller('styleCtrl', ['$scope', '$anchorScroll', '$location', function ($scope, $anchorScroll, $location) {
+    $scope.styles = styles;
+
+    /* Initialize default page settings */
+    $scope.buttonAlign = "center";
+    $scope.collapseDefault = "open";
+    $scope.textStyle = "instructions";
+	$scope.textStyleCompact = false;
+    $scope.userMessageCallback = false;
+    $scope.userMessageCompact = false;
+    $scope.userMessageDismiss = true;
+    $scope.userMessageType = "success";
+	$scope.userMessageVisible = { show: true };
+
+    /* Utility functions */
+    $scope.scrollTo = function (id) {
+        var old = $location.hash();
+        $location.hash(id);
+        $anchorScroll();
+        //reset to old to keep any additional routing logic from kicking in
+        $location.hash(old);
+    };
+
+} ]);
